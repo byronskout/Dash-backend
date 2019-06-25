ef9f7ebe24317324de6f3bc59477d26f617e2229
github username <vyskoutaris@hotmail.com>
2019-06-25 14:14:25 +0100

    Migration fixes, routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
+  resources :users, only: [:index, :create]
+
+  post "/auth/create", to: "auth#create"
+  get "/auth/show", to: "auth#show"
+
 end
